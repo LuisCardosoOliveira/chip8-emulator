5399f750ac0c667af874c2923f75474d91f2a052
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2022-03-15 15:46:24 +0100

    feat: implement CHIP8 call and return functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 struct Cpu {
     registers: [u8; 16],
     position_in_memory: usize,
-    memory: [u8; 0x1000], // represents 4096 bytes of RAM
+    // represents 4096 bytes of RAM
+    memory: [u8; 4096],
+    // stack's max height is 16. After 16 nested function calls, the program
+    // encounters a stack overflow.
+    stack: [u16; 16],
+    stack_pointer: usize,
 }
 
 impl Cpu {
@@ -11,7 +16,7 @@ impl Cpu {
         let op_byte2 = self.memory[p + 1] as u16;
 
         // This creates a u16 opcode combining two values
-        // from `memory`with the logical OR operation.
+        // from `memory` with the logical OR operation.
         // These need to be cast as u16 to start with
         // otherwise, the left shift sets all of the bits to 0.
         op_byte1 << 8 | op_byte2
@@ -30,15 +35,49 @@ impl Cpu {
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = (opcode & 0x000F) as u8;
 
+            let nnn = opcode & 0x0FFF;
+
             match (c, x, y, d) {
                 // terminate execution when opcode === 0x0000
                 (0, 0, 0, 0) => {
                     return;
                 }
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
         }
+    }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow!")
+        }
+
+        // Adds the current `position_in_memory` to the stack.
+        stack[sp] = self.position_in_memory as u16;
+
+        // Increments `self.stack_pointer` to prevent `self.position_in_memory`
+        // from being overwritten until it needs to be accessed again in a
+        // subsequent return
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow");
+        }
+
+        self.stack_pointer -= 1;
+
+        // Jumps to position in memory where an earlier call was made
+        let call_addr = self.stack[self.stack_pointer];
+        self.position_in_memory = call_addr as usize;
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
@@ -62,29 +101,36 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         position_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
     let mem = &mut cpu.memory;
-    // Loads opcode 0x8014 which adds register 1 to register 0
-    mem[0] = 0x80;
-    mem[1] = 0x14;
+    // Sets opcode to 0x2100: CALL the function at 0x100
+    mem[0x000] = 0x21;
+    mem[0x001] = 0x00;
+    // Sets opcode to 0x2100: CALL the function at 0x100
+    mem[0x002] = 0x21;
+    mem[0x003] = 0x00;
+    // Sets opcode to 0x0000: HALT
+    mem[0x004] = 0x00;
+    mem[0x005] = 0x00;
 
-    // Loads opcode 0x8024 which adds register 2 to register 0
-    mem[2] = 0x80;
-    mem[3] = 0x24;
+    // Sets opcode to 0x8014: ADD register 1's value to register °
+    mem[0x100] = 0x80;
+    mem[0x101] = 0x14;
+    // Sets opcode to 0x8014: ADD register 1's value to register °
+    mem[0x102] = 0x80;
+    mem[0x103] = 0x14;
 
-    // Loads opcode 0x8034 which adds register 3 to register 0
-    mem[4] = 0x80;
-    mem[5] = 0x34;
+    mem[0x104] = 0x00;
+    mem[0x105] = 0xEE;
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
-
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    assert_eq!(cpu.registers[0], 45);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
